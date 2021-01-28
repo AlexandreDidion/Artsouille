f8965e088fcb88f06aa5186dbe5e0a88a01a4343
Laurene Eppelle <laurene.eppelle@gmail.com>
2021-01-28 19:14:39 +0100

    Created exhibition Model + validations

---
+++ app/models/exhibition.rb
@@ -0,0 +1,9 @@
+class Exhibition < ApplicationRecord
+  belongs_to :collab
+
+  validates :name, presence: true, uniqueness: true
+  validates :category, inclusion: { in: ["visual", "sculpture", "performance", "music"] }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :address, presence: true
+end
