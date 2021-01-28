45108925984a3992f88623d9c407c0c6b53d8277
Kliti <hamatajkliti@gmail.com>
2021-01-28 19:57:31 +0100

    added new columns to users schema + user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+  has_many :work_of_arts, dependent: :destroy
+  has_many :users_collab, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
