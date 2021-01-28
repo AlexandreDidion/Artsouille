c959303c60136d9f1372f28c8df6046e6d059696
Kliti <hamatajkliti@gmail.com>
2021-01-28 20:36:39 +0100

    changed the name from favorites to work_of_art_favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :favorites, dependent: :destroy
+  has_many :work_of_art_favorites, dependent: :destroy
   has_many :work_of_arts, dependent: :destroy
   has_many :users_collab, dependent: :destroy
   # Include default devise modules. Others available are:
