fc30c898a9804e7cea61e4d3a42b78e9444f5a0e
Kliti <hamatajkliti@gmail.com>
2021-01-28 21:38:27 +0100

    typo fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :work_of_art_favorites, dependent: :destroy
   has_many :work_of_arts, dependent: :destroy
-  has_many :collabs, through: :users_collabs dependent: :destroy
+  has_many :collabs, through: :users_collabs, dependent: :destroy
   
   validates :art_type, inclusion: { in: ["abstract", "animation", "calligraphy", "design", "digital", "musician", "painter", "performer", "photography", "sculupture", "visual effects", "emerging"]}
   validates :username, :first_name, :last_name, presence: true
