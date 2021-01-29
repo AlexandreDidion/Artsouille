e9ab177efcabac086d0688c86c88ba09ce773170
Kliti <hamatajkliti@gmail.com>
2021-01-29 14:52:03 +0100

    created seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :collabs, through: :users_collabs, dependent: :destroy
   
   validates :art_type, inclusion: { in: ["abstract", "animation", "calligraphy", "design", "digital", "musician", "painter", "performer", "photography", "sculupture", "visual effects", "emerging"]}
-  validates :username, :first_name, :last_name, presence: true
+  validates :username, uniqueness: true, presence: true
+  validates :first_name, :last_name, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
