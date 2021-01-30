2871d5c570c5999ac36cb2f2307c2b08ac57abe2
Alexandre Didion <66737841+AlexandreDidion@users.noreply.github.com>
2021-01-30 10:16:19 +0100

    Update collab.rb

--- app/models/collab.rb
+++ app/models/collab.rb
@@ -1,5 +1,5 @@
 class Collab < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :users_collabs, dependent: :destroy
-  has_many :users, through: :users_collabs, dependent: :destroy
+  has_many :users, through: :users_collabs
 end
