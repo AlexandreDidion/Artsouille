2fbe158f0e9969b012d34c1aafb34a62709a78f2
Alexandre Didion <ale.didion@gmail.com>
2021-01-30 15:54:00 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users, only: [:show, :edit, :update]
   root to: 'pages#home'
-  resources :collabs
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :work_of_arts, only: [:show, :edit, :update, :delete ]
+  end
+  resources :work_of_arts, only: [:new, :create, :index]
+  resources :collabs do
+    resources :exhibitions, only: [:new, :create, :show, :edit, :update, :delete]
+  end
+  resources :exhibitions, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
