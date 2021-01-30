44be721d17cb96616c1231cd674eb93f8f5336bb
Alexandre Didion <66737841+AlexandreDidion@users.noreply.github.com>
2021-01-30 15:38:18 +0100

    Revert "create existing links for navigation"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
   root to: 'pages#home'
   resources :collabs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
