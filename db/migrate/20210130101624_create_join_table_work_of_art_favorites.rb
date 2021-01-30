ffffdf6cd23c64674eb247bf3fdc901093f4d65b
Laurene Eppelle <laurene.eppelle@gmail.com>
2021-01-30 11:32:22 +0100

    added indices

--- db/migrate/20210130101624_create_join_table_work_of_art_favorites.rb
+++ db/migrate/20210130101624_create_join_table_work_of_art_favorites.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableWorkOfArtFavorites < ActiveRecord::Migration[6.0]
   def change
     create_join_table :users, :work_of_arts do |t|
-      # t.index [:user_id, :work_of_art_id]
-      # t.index [:work_of_art_id, :user_id]
+      t.index [:user_id, :work_of_art_id]
+      t.index [:work_of_art_id, :user_id]
     end
   end
 end
