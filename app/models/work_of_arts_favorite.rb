20a25fa8b572893c49f0d422369cb0e466fffb68
Laurene Eppelle <laurene.eppelle@gmail.com>
2021-01-30 14:38:23 +0100

    dependancies

--- app/models/work_of_arts_favorite.rb
+++ app/models/work_of_arts_favorite.rb
@@ -1,4 +1,4 @@
 class WorkOfArtsFavorite < ApplicationRecord
-  belongs_to :work_of_art, dependent: :destroy
-  belongs_to :user, dependent: :destroy
+  belongs_to :work_of_art
+  belongs_to :user
 end
