0f37768b2e86ebc86d3d42ffdd6431aeabca83c1
Alexandre Didion <ale.didion@gmail.com>
2021-01-31 18:30:16 +0100

    Add feature My art

--- app/controllers/work_of_arts_controller.rb
+++ app/controllers/work_of_arts_controller.rb
@@ -2,17 +2,19 @@ class WorkOfArtsController < ApplicationController
   before_action :set_work_of_arts, only: [:show]
 
   def index
-    @work_of_arts = WorkOfArt.all
+    if params[:query] == 'my_arts'
+      @work_of_arts = WorkOfArt.where('user_id = ?', current_user)
+    else
+      @work_of_arts = WorkOfArt.all
+    end
   end
 
   def show
   end
-
 
   private
 
   def set_work_of_arts
     @work_of_art = WorkOfArt.find(params[:id])
   end
-
 end
