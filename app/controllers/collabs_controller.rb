24a26b38c9da013c42e384f261bdcb5ce1e39427
Alexandre Didion <ale.didion@gmail.com>
2021-02-05 14:47:18 +0100

    Basic front and cannot modify if not member of a collab + My collabs in navbar

--- app/controllers/collabs_controller.rb
+++ app/controllers/collabs_controller.rb
@@ -3,7 +3,7 @@ class CollabsController < ApplicationController
 
   def index
     if params[:query] == 'my_collabs'
-      @collabs = Collab.where('user_id = ?', current_user)
+      @collabs = Collab.joins(:users_collabs).where('user_id = ?', current_user)
     else
       @collabs = Collab.all
     end
