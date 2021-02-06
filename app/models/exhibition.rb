269d7be115dc610fa986732b1e3cf1828b4c890d
Laurene Eppelle <laurene.eppelle@gmail.com>
2021-02-06 16:11:24 +0100

    add migration

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -7,4 +7,7 @@ class Exhibition < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_104026) do
+ActiveRecord::Schema.define(version: 2021_02_06_150842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2021_02_06_104026) do
     t.bigint "collab_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["collab_id"], name: "index_exhibitions_on_collab_id"
   end
 
@@ -89,11 +91,6 @@ ActiveRecord::Schema.define(version: 2021_02_06_104026) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["collab_id"], name: "index_users_collabs_on_collab_id"
     t.index ["user_id"], name: "index_users_collabs_on_user_id"
-  end
-
-  create_table "users_work_of_arts", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "work_of_art_id", null: false
   end
 
   create_table "work_of_arts", force: :cascade do |t|
