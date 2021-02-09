1caa2e7214e724b39e2433d2389df0df501c6d4e
Alexandre Didion <ale.didion@gmail.com>
2021-02-09 18:52:02 +0100

    search bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,21 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
   def index
-    @users = User.all
+    if params[:search].present?
+      sql_query = " \
+          users.username @@ :query \
+          OR users.first_name @@ :query \
+          OR users.last_name @@ :query \
+          OR users.country @@ :query \
+          OR users.city @@ :query \
+          OR users.art_type @@ :query \
+          OR work_of_arts.name @@ :query \
+          OR work_of_arts.description @@ :query \
+        "
+      @users = User.joins(:work_of_arts).where(sql_query, query: "%#{params[:search][:name]}%")
+    else
+      @users = User.all
+    end
   end
 
   def show; end
