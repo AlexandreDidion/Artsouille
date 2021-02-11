c36d2e967dcc93f6bdb2d3eb322af426d763f15b
Laurene Eppelle <laurene.eppelle@gmail.com>
2021-02-11 21:30:51 +0100

    add column to collab

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_02_10_132805) do
-
+ActiveRecord::Schema.define(version: 2021_02_11_202650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_02_10_132805) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "exhibitions", force: :cascade do |t|
