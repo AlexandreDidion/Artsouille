f942579eba6cdcbda37489ca7aa97614df2cb9d8
Kliti <hamatajkliti@gmail.com>
2021-02-13 17:47:36 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_120407)
+ActiveRecord::Schema.define(version: 2021_02_13_133828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,13 +41,6 @@ ActiveRecord::Schema.define(version: 2021_02_13_120407)
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
-  end
-
-  create_table "conversations", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "receiver_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "exhibitions", force: :cascade do |t|
@@ -89,16 +82,6 @@ ActiveRecord::Schema.define(version: 2021_02_13_120407)
     t.integer "followee_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text "content"
-    t.bigint "conversation_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "to_do_items", force: :cascade do |t|
@@ -161,14 +144,23 @@ ActiveRecord::Schema.define(version: 2021_02_13_120407)
     t.index ["user_id"], name: "index_work_of_arts_on_user_id"
   end
 
+  create_table "work_of_arts_favorites", force: :cascade do |t|
+    t.bigint "work_of_art_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_work_of_arts_favorites_on_user_id"
+    t.index ["work_of_art_id"], name: "index_work_of_arts_favorites_on_work_of_art_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "exhibitions", "collabs"
-  add_foreign_key "messages", "conversations"
-  add_foreign_key "messages", "users"
   add_foreign_key "to_do_items", "to_dos"
   add_foreign_key "to_dos", "collabs"
   add_foreign_key "user_collab_relationships", "collabs"
   add_foreign_key "user_collab_relationships", "users"
   add_foreign_key "work_of_arts", "collabs"
   add_foreign_key "work_of_arts", "users"
+  add_foreign_key "work_of_arts_favorites", "users"
+  add_foreign_key "work_of_arts_favorites", "work_of_arts"
 end
