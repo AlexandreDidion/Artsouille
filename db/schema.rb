cbf36fc77c7473cf79b7e41b64e5948b9a518783
Alexandre Didion <ale.didion@gmail.com>
2021-02-16 20:32:06 +0100

    remove duplicates in results

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_120407)
+ActiveRecord::Schema.define(version: 2021_02_13_133828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -161,6 +161,15 @@ ActiveRecord::Schema.define(version: 2021_02_13_120407)
     t.index ["user_id"], name: "index_work_of_arts_on_user_id"
   end
 
+  create_table "work_of_arts_favorites", force: :cascade do |t|
+    t.bigint "work_of_art_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_work_of_arts_favorites_on_user_id"
+    t.index ["work_of_art_id"], name: "index_work_of_arts_favorites_on_work_of_art_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "exhibitions", "collabs"
   add_foreign_key "messages", "conversations"
@@ -171,4 +180,6 @@ ActiveRecord::Schema.define(version: 2021_02_13_120407)
   add_foreign_key "user_collab_relationships", "users"
   add_foreign_key "work_of_arts", "collabs"
   add_foreign_key "work_of_arts", "users"
+  add_foreign_key "work_of_arts_favorites", "users"
+  add_foreign_key "work_of_arts_favorites", "work_of_arts"
 end
