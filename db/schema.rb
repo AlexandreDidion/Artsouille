dfc848a7b27fc657a3768ee8c748911b735b57ea
Laurene Eppelle <laurene.eppelle@gmail.com>
2021-02-18 16:05:42 +0100

    fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_133828) do
+ActiveRecord::Schema.define(version: 2021_02_18_132146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,7 +105,7 @@ ActiveRecord::Schema.define(version: 2021_02_13_133828) do
     t.bigint "collab_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "status", default: 1
+    t.integer "status", default: 0
     t.index ["collab_id"], name: "index_user_collab_relationships_on_collab_id"
     t.index ["user_id"], name: "index_user_collab_relationships_on_user_id"
   end
